d36acfc086fb1c72e5f08c85b238a012b57ff7f3
Jennifer <jennifacarey@gmail.com>
2021-03-07 17:52:27 +0000

    create err router for 404

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/indexRouter");
+const errRouter = require("./routes/errRouter");
 
 const getWeather = require("./lib/getWeather");
 
@@ -24,10 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", ".hbs");
 
 app.use("/", indexRouter);
-
-app.get("*", (req, res) => {
-   res.render("404");
-})
+app.use("*", errRouter);
 
 app.listen(3000, () => {
    console.log("Listening to port 3000");
