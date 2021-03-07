605abd43816da2efbf92e67b05dd27fbfebe2d25
Jennifer <jennifacarey@gmail.com>
2021-03-07 17:46:30 +0000

    create index router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const hbs = require("express-handlebars");
 const path = require("path");
 const bodyParser = require("body-parser");
 
+const indexRouter = require("./routes/indexRouter");
+
 const getWeather = require("./lib/getWeather");
 
 const app = express();
@@ -21,47 +23,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", ".hbs");
 
-app.get("/", async(req, res) => {
-   let data = await getWeather(`${process.env.CITY}`, `${process.env.CODE}`);
-   let city = data.name;
-   let country = data.sys.country;
-   let icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-   let temp = data.main.temp;
-   let description = data.weather[0].description;
-   let feelsLike = data.main.feels_like;
-   res.render("index", {
-      city, 
-      country, 
-      icon,
-      data: {temp, description, feelsLike}, 
-      listExists: true
-   });
-});
-
-app.post("/", async(req, res) => {
-   let location = req.body.location;
-   let code = req.body.countryCode;
-   let data = await getWeather(location, code);
-   if (data.cod == "404") {
-      res.render("index", {
-         err: `We can't find that location, please try again.`
-      });
-      return;
-   }
-   let city = data.name;
-   let country = data.sys.country;
-   let icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-   let temp = data.main.temp;
-   let description = data.weather[0].description;
-   let feelsLike = data.main.feels_like;
-   res.render("index", {
-      city, 
-      country, 
-      icon,
-      data: {temp, description, feelsLike}, 
-      listExists: true
-   });
-});
+app.use("/", indexRouter);
 
 app.get("*", (req, res) => {
    res.render("404");
