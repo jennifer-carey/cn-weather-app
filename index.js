7b3889c23ca4dd1adb4e03a82bdc65f3be561840
Jennifer <jennifacarey@gmail.com>
2021-03-07 11:06:05 +0000

    Add express server files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("hbs", hbs({
+   defaultLayout: "layout",
+   extname: ".hbs"
+}));
+
+app.set("view engine", ".hbs");
+
+app.get("/", (req, res) => {
+   res.render("index");
+});
+
+
+app.listen(3000, () => {
+   console.log("Listening to port 3000");
+});
