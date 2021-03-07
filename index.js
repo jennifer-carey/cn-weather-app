e2b409d3e0a17a2bdc3ca9dcdf9d276ea7192fe0
Jennifer <jennifacarey@gmail.com>
2021-03-07 16:08:28 +0000

    add weather icon

--- index.js
+++ index.js
@@ -25,12 +25,14 @@ app.get("/", async(req, res) => {
    let data = await getWeather(`${process.env.CITY}`, `${process.env.CODE}`);
    let city = data.name;
    let country = data.sys.country;
+   let icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
    let temp = data.main.temp;
    let description = data.weather[0].description;
    let feelsLike = data.main.feels_like;
    res.render("index", {
       city, 
       country, 
+      icon,
       data: {temp, description, feelsLike}, 
       listExists: true
    });
@@ -48,12 +50,14 @@ app.post("/", async(req, res) => {
    }
    let city = data.name;
    let country = data.sys.country;
+   let icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
    let temp = data.main.temp;
    let description = data.weather[0].description;
    let feelsLike = data.main.feels_like;
    res.render("index", {
       city, 
       country, 
+      icon,
       data: {temp, description, feelsLike}, 
       listExists: true
    });
